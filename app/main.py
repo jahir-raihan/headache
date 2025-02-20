3984c28612dfaa972254256e6a055bf2c80834a4
Jahir Raihan Joy <raihan.joy10@gmail.com>
2025-02-20 10:59:10 +0600

    Possible fix of packaging error

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from .db import create_db_and_tables
-from .models.database.folder_and_files import Folder, Document
+from app.db import create_db_and_tables
+from app.models.database.folder_and_files import Folder, Document
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import folder_and_files
+from app.routers import folder_and_files
 
 
 # Lifespan function
